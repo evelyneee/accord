cf0591c6ae8fde13b7305b7fe987a40c1f6f4029
evelyn <charlesdelorean1@icloud.com>
2021-06-14 14:29:34 -0400

    remove something dumb

--- build.sh
+++
@@ -1,4 +0,0 @@
-xcodebuild
-open /Users/althio/Library/Developer/Xcode/DerivedData/Helselia-cnjurocrcnbfsufzvduwjdaakluu/Build/Products/Debug/Helselia.app
-echo "compile done, launching compiled build now"
-echo ""
