4874ad9b49b7c4f7837d14a139b3f13ff0cd2d81
evelyn <charlesdelorean1@icloud.com>
2021-06-20 13:11:41 -0400

    [Accord] Remove iOS, properly

---
+++ Accord/discordlib/Messages.swift
@@ -0,0 +1,75 @@
+//
+//  Messages.swift
+//  Accord
+//
+//  Created by evelyn on 2021-03-07.
+//
+
+import Foundation
+import SwiftUI
+
+final class ParseMessages {
+    static var shared = ParseMessages()
+    
+    func getArray(forKey: String, messageDictionary: [[String:Any]]) -> [Any] {
+        var returnItem: [Any] = []
+        for message in messageDictionary {
+            switch forKey {
+            case "author":
+                returnItem.append("\((message["author"] as! Dictionary<String, Any>)["username"] ?? "error")#\((message["author"] as! Dictionary<String, Any>)["discriminator"] ?? "0000")" )
+            case "avatar":
+                returnItem.append("https://cdn.discordapp.com/avatars/\((message["author"] as! Dictionary<String, Any>)["id"]!)/\((message["author"] as! Dictionary<String, Any>)["avatar"]!).png?size=80")
+            case "user_id":
+                returnItem.append((message["author"] as! Dictionary<String, Any>)["id"] ?? "error")
+            default:
+                returnItem.append(message[forKey] ?? "")
+            }
+        }
+        return returnItem
+    }
+}
+
+struct ImageWithURL: View {
+    
+    @ObservedObject var imageLoader: ImageLoaderAndCache
+
+    init(_ url: String) {
+        imageLoader = ImageLoaderAndCache(imageURL: url)
+    }
+
+    var body: some View {
+        Image(nsImage: (NSImage(data: self.imageLoader.imageData) ?? NSImage(named: "")) ?? NSImage())
+              .resizable()
+              .clipped()
+    }
+}
+
+struct Attachment: View {
+    
+    @ObservedObject var imageLoader: ImageLoaderAndCache
+
+    init(_ url: String) {
+        imageLoader = ImageLoaderAndCache(imageURL: url)
+    }
+
+    var body: some View {
+        Image(nsImage: (NSImage(data: self.imageLoader.imageData) ?? NSImage(named: "")) ?? NSImage())
+              .resizable()
+              .scaledToFit()
+    }
+}
+
+class ImageLoaderAndCache: ObservableObject {
+    
+    @Published var imageData = Data()
+    
+    init(imageURL: String) {
+        net.requestData(url: imageURL, token: nil, json: false, type: .GET, bodyObject: [:]) { success, data in
+            if success {
+                DispatchQueue.main.async {
+                    self.imageData = data ?? Data()
+                }
+            }
+        }
+    }
+}
