c96ab5ecc8f5449b7ca8cf98b1965a7f27505cbf
evelyneee <delorean3344@gmail.com>
2022-02-04 13:20:25 -0500

    remove discord ios code

--- Accord/UI/Base/LoginView.swift
+++ Accord/UI/Base/LoginView.swift
@@ -298,41 +298,6 @@ final class ScriptHandler: NSObject, WKScriptMessageHandler {
     }
 }
 
-/*
- - extracted from Discord iOS v100
-
- function documentReady() {
-   var PAGE_BG_COLOR = RECAPTCHA_THEME == 'dark' ? '#222' : '#fff';
-   document.body.style.backgroundColor = PAGE_BG_COLOR;
-   showCaptcha(document.body.firstElementChild);
- }
-
- function showCaptcha(el) {
-   try {
-     grecaptcha.render(el, {
-       sitekey: RECAPTCHA_SITE_KEY,
-       theme: RECAPTCHA_THEME,
-       callback: captchaSolved,
-       'expired-callback': captchaExpired,
-     });
-
-     window.webkit.messageHandlers.reCaptcha.postMessage(['didLoad']);
-   } catch (_) {
-     window.setTimeout(function() {
-       showCaptcha(el);
-     }, 50);
-   }
- }
-
- function captchaSolved(response) {
-   window.webkit.messageHandlers.reCaptcha.postMessage(['didSolve', response]);
- }
-
- function captchaExpired(response) {
-   window.webkit.messageHandlers.reCaptcha.postMessage(['didExpire']);
- }
- */
-
 extension CaptchaViewControllerSwiftUI {
     private var generateHTML: String {
         """
