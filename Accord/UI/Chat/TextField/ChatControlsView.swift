a887cb997ddf2209d010e859dfb42905eb7dd226
ConnectedR <44319023+ConnectedR@users.noreply.github.com>
2022-06-16 17:12:38 +0100

    Fix PopoverProfileView padding, add attachment preview

--- Accord/UI/Chat/TextField/ChatControlsView.swift
+++ Accord/UI/Chat/TextField/ChatControlsView.swift
@@ -77,6 +77,40 @@ struct ChatControls: View {
 //            }
 //        }
 //    }
+    
+    var mediaView: some View {
+        ZStack(alignment: .topTrailing) {
+            if let fileUpload = fileUpload, let nsImage = NSImage(data: fileUpload) {
+                Image(nsImage: nsImage)
+                    .resizable()
+                    .scaledToFit()
+                    .cornerRadius(5)
+                    .frame(height: 180)
+                
+            } else {
+                ZStack(alignment: .center) {
+                    Image(systemName: "doc")
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundColor(Color.white.opacity(0.4))
+                        .frame(width: 48, height: 48)
+                }
+                .frame(width: 180, height: 180)
+                .background(Color.black.opacity(0.2))
+                .cornerRadius(5)
+            }
+            
+            Image(systemName: "xmark.circle.fill")
+                .resizable()
+                .symbolRenderingMode(.palette)
+                .foregroundStyle(.black, Color.white.opacity(0.5))
+                .frame(width: 22, height: 22)
+                .onTapGesture {
+                    self.fileUpload = nil
+                }
+                .padding(4)
+        }.frame(maxWidth: .infinity, alignment: .leading)
+    }
 
     var matchedUsersView: some View {
         MatchesView(
@@ -275,6 +309,10 @@ struct ChatControls: View {
                         }
                         .padding(.bottom, 7)
                     }
+                    if fileUpload != nil {
+                        mediaView
+                        Divider().padding(.bottom, 7)
+                    }
                     HStack {
                         fileImportButton
                         Divider().frame(height: 20)
@@ -283,15 +321,6 @@ struct ChatControls: View {
                             nitrolessButton
                         }
                         emotesButton
-                        HStack {
-                            if fileUpload != nil {
-                                Image(systemName: "doc.fill")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 17.5, height: 17.5)
-                                    .foregroundColor(Color.secondary)
-                            }
-                        }
                     }
                     .disabled(!self.permissions.contains(.sendMessages))
                     .onReceive(viewModel.$textFieldContents) { [weak viewModel] _ in
--- Accord/UI/Profile/PopoverProfileView.swift
+++ Accord/UI/Profile/PopoverProfileView.swift
@@ -25,7 +25,7 @@ struct RolesView: View {
             ForEach(self.tags, id: \.self) { tag in
                 self.item(for: tag)
                     .alignmentGuide(.leading, computeValue: { d in
-                        if abs(width - d.width) > 276 {
+                        if abs(width - d.width) > 262 {
                             width = 0
                             height -= d.height
                         }
@@ -241,28 +241,11 @@ struct PopoverProfileView: View {
                                 }
                             }
                         }
-                        PopoverProfileViewButton(
-                            label: "Call",
-                            symbolName: "phone.fill"
-                        ) {
-                            // TODO: voice chat
-                        }
-                        PopoverProfileViewButton(
-                            label: "Video call",
-                            symbolName: "camera.fill"
-                        ) {
-                            // TODO: video call
-                        }
-                        PopoverProfileViewButton(
-                            label: "Add Friend",
-                            symbolName: "person.fill.badge.plus"
-                        ) {
-                            // TODO: check add friend
-                        }
                     }
+                    .frame(maxWidth: .infinity)
                     .transition(AnyTransition.opacity)
                 }
-                .padding()
+                .padding(14)
                 .background(Color(NSColor.windowBackgroundColor))
             }
         }
