167d5a54e4579ae5f339cc24f88e8b1d232ebb1c
Évelyne <charlesdelorean1@icloud.com>
2022-10-22 19:45:01 -0400

    fix: last one!!

--- Accord/UI/Chat/ChannelView/ChannelView.swift
+++ Accord/UI/Chat/ChannelView/ChannelView.swift
@@ -399,11 +399,13 @@ struct ChannelView: View {
         .toolbar {
             ToolbarItem(placement: .navigation) {
                 HStack {
-                    Button(action: {
-                        NSApp.keyWindow?.firstResponder?.tryToPerform(#selector(NSSplitViewController.toggleSidebar(_:)), with: nil)
-                    }, label: {
-                        Image(systemName: "sidebar.leading")
-                    })
+                    if #unavailable(macOS 13.0) {
+                        Button(action: {
+                            NSApp.keyWindow?.firstResponder?.tryToPerform(#selector(NSSplitViewController.toggleSidebar(_:)), with: nil)
+                        }, label: {
+                            Image(systemName: "sidebar.leading")
+                        })
+                    }
                     if guildName == "Direct Messages" {
                         Text("@")
                             .font(.title3)
