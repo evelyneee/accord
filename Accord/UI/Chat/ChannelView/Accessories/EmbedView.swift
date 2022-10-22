565b51bafc75437d74522b9b58399c07349e157c
Évelyne <charlesdelorean1@icloud.com>
2022-10-22 16:07:51 -0400

    feat: traditional media embed

--- Accord/UI/Chat/ChannelView/Accessories/EmbedView.swift
+++ Accord/UI/Chat/ChannelView/Accessories/EmbedView.swift
@@ -56,92 +56,124 @@ struct EmbedView: View, Equatable {
         }
     }
     
+    @ViewBuilder
+    var mediaEmbed: some View {
+        if let image = embed.image {
+            Attachment(image.url, size: CGSize(width: image.width ?? 400, height: image.width ?? 300))
+                .equatable()
+                .cornerRadius(7)
+                .maxFrame(width: 380, height: 300, originalWidth: image.width ?? 0, originalHeight: image.height ?? 0)
+                .padding(.vertical, 2)
+        }
+        if let video = embed.video,
+           let urlString = video.proxy_url ?? video.url,
+           let url = URL(string: urlString)
+        {
+            VideoPlayerController(url: url)
+                .cornerRadius(5)
+                .maxFrame(width: 380, height: 300, originalWidth: video.width ?? 0, originalHeight: video.height ?? 0)
+                .padding(.vertical, 2)
+        } else if let image = embed.thumbnail {
+            Attachment(image.url)
+                .equatable()
+                .maxFrame(width: 380, height: 300, originalWidth: image.width ?? 0, originalHeight: image.height ?? 0)
+                .cornerRadius(7)
+                .padding(.vertical, 2)
+        }
+    }
+    
+    @ViewBuilder
     var body: some View {
-        GroupBox {
-            HStack(spacing: 0) {
-                colorLine.padding(.leading, -5).padding(.vertical, -5)
-                
-                VStack(alignment: .leading) {
-                    if let author = embed.author {
-                        HStack {
-                            if let iconURL = author.proxy_icon_url ?? author.icon_url {
-                                Attachment(iconURL, size: CGSize(width: 48, height: 48))
-                                    .equatable()
-                                    .frame(width: 21, height: 21)
-                                    .clipShape(Circle())
-                            }
-                            if let urlString = author.url, let url = URL(string: urlString) {
-                                Button(action: {
-                                    NSWorkspace.shared.open(url)
-                                }) {
-                                    Text(author.name)
-                                        .fontWeight(.semibold)
-                                        .font(.system(size: 14.5))
-                                        .foregroundColor(Color.primary.opacity(0.85))
-                                }
-                                .buttonStyle(BorderlessButtonStyle())
-                            } else {
-                                Text(author.name)
-                                    .fontWeight(.medium)
-                                    .font(.system(size: 14.5))
-                            }
-                        }
-                        .padding(.bottom, 2)
-                    }
+        if embed.type == "gifv" || embed.type == "image" || embed.type == "video" {
+            self.mediaEmbed
+        } else {
+            GroupBox {
+                HStack(spacing: 0) {
+                    colorLine.padding(.leading, -5).padding(.vertical, -5)
+                    
                     VStack(alignment: .leading) {
-                        if let title = embed.title {
-                            Text(title)
-                                .fontWeight(.semibold)
-                                .font(.system(size: 13.5))
-                                .padding(.vertical, 2)
-                                .lineLimit(0)
+                        if let author = embed.author {
+                            HStack {
+                                if let iconURL = author.proxy_icon_url ?? author.icon_url {
+                                    Attachment(iconURL, size: CGSize(width: 48, height: 48))
+                                        .equatable()
+                                        .frame(width: 21, height: 21)
+                                        .clipShape(Circle())
+                                }
+                                if let urlString = author.url, let url = URL(string: urlString) {
+                                    Button(action: {
+                                        NSWorkspace.shared.open(url)
+                                    }) {
+                                        Text(author.name)
+                                            .fontWeight(.semibold)
+                                            .font(.system(size: 14.5))
+                                            .foregroundColor(Color.primary.opacity(0.85))
+                                    }
+                                    .buttonStyle(BorderlessButtonStyle())
+                                } else {
+                                    Text(author.name)
+                                        .fontWeight(.medium)
+                                        .font(.system(size: 14.5))
+                                }
+                            }
+                            .padding(.bottom, 2)
                         }
-                        if let description = embed.description {
-                            AsyncMarkdown(description, linkShortening: true)
-                                .lineSpacing(3)
-                                .padding(.vertical, 2)
-                        }
-                    }
-                    if let image = embed.image {
-                        Attachment(image.url, size: CGSize(width: image.width ?? 400, height: image.width ?? 300))
-                            .equatable()
-                            .cornerRadius(7)
-                            .maxFrame(width: 380, height: 300, originalWidth: image.width ?? 0, originalHeight: image.height ?? 0)
-                            .padding(.vertical, 2)
-                    }
-                    if let video = embed.video,
-                       let urlString = video.proxy_url ?? video.url,
-                       let url = URL(string: urlString)
-                    {
-                        VideoPlayerController(url: url)
-                            .cornerRadius(5)
-                            .maxFrame(width: 380, height: 300, originalWidth: video.width ?? 0, originalHeight: video.height ?? 0)
-                            .padding(.vertical, 2)
-                    } else if let image = embed.thumbnail {
-                        Attachment(image.url)
-                            .equatable()
-                            .maxFrame(width: 380, height: 300, originalWidth: image.width ?? 0, originalHeight: image.height ?? 0)
-                            .cornerRadius(7)
-                            .padding(.vertical, 2)
-                    }
-                    if let fields = embed.fields {
-                        GridStack(fields, rowAlignment: .leading, columns: 4) { field in
-                            VStack(alignment: .leading) {
-                                Text(field.name)
-                                    .font(.system(size: 12))
+                        VStack(alignment: .leading) {
+                            if let title = embed.title {
+                                Text(title)
                                     .fontWeight(.semibold)
+                                    .font(.system(size: 13.5))
+                                    .padding(.vertical, 2)
                                     .lineLimit(0)
-                                AsyncMarkdown(field.value, linkShortening: true)
-                                    .equatable()
-                                    .font(.system(size: 12))
+                            }
+                            if let description = embed.description {
+                                AsyncMarkdown(description, linkShortening: true)
+                                    .lineSpacing(3)
+                                    .padding(.vertical, 2)
                             }
                         }
-                        .equatable()
-                        .padding(.vertical, 2)
+                        if let image = embed.image {
+                            Attachment(image.url, size: CGSize(width: image.width ?? 400, height: image.width ?? 300))
+                                .equatable()
+                                .cornerRadius(7)
+                                .maxFrame(width: 380, height: 300, originalWidth: image.width ?? 0, originalHeight: image.height ?? 0)
+                                .padding(.vertical, 2)
+                        }
+                        if let video = embed.video,
+                           let urlString = video.proxy_url ?? video.url,
+                           let url = URL(string: urlString)
+                        {
+                            VideoPlayerController(url: url)
+                                .cornerRadius(5)
+                                .maxFrame(width: 380, height: 300, originalWidth: video.width ?? 0, originalHeight: video.height ?? 0)
+                                .padding(.vertical, 2)
+                        } else if let image = embed.thumbnail {
+                            Attachment(image.url)
+                                .equatable()
+                                .maxFrame(width: 380, height: 300, originalWidth: image.width ?? 0, originalHeight: image.height ?? 0)
+                                .cornerRadius(7)
+                                .padding(.vertical, 2)
+                        }
+                        if let fields = embed.fields {
+                            GridStack(fields, rowAlignment: .leading, columns: 4) { field in
+                                VStack(alignment: .leading) {
+                                    Text(field.name)
+                                        .font(.system(size: 12))
+                                        .fontWeight(.semibold)
+                                        .lineLimit(0)
+                                    AsyncMarkdown(field.value, linkShortening: true)
+                                        .equatable()
+                                        .font(.system(size: 12))
+                                }
+                            }
+                            .equatable()
+                            .padding(.vertical, 2)
+                        }
                     }
+                    .padding(5)
                 }
-                .padding(5)
             }
+
         }
     }
 }
