709f5d0b4eccdf23e9f0643a43ead37b214b9ed3
Évelyne <charlesdelorean1@icloud.com>
2022-10-24 20:17:52 -0400

    re enable the link hooks

--- Accord/Backend/Media/GifManager.swift
+++ Accord/Backend/Media/GifManager.swift
@@ -12,9 +12,12 @@ final class Gif: NSImage {
     var animatedImages: [NSImage]?
 
     convenience init?(data: Data) {
+        
+        
         guard let source = CGImageSourceCreateWithData(data as CFData, nil),
-              let metadata = CGImageSourceCopyPropertiesAtIndex(source, 0, nil),
-              let delayTime = (((metadata as NSDictionary)["{GIF}"] as? NSDictionary) ?? ((metadata as NSDictionary)["{PNG}"] as? NSDictionary))?["DelayTime"] as? Double else { return nil }
+              let metadata = CGImageSourceCopyPropertiesAtIndex(source, 0, nil) else { return nil }
+        let gifTime = ((metadata as NSDictionary)["{GIF}"] as? NSDictionary)?["DelayTime"] as? Double
+        let pngTime = (((metadata as NSDictionary)["{PNG}"] as? NSDictionary)?["DelayTime"] as? Double ?? 0.0) / 2.2
         var images: [NSImage] = .init()
         let imageCount = CGImageSourceGetCount(source)
         let width = (metadata as NSDictionary)["PixelWidth"] as? Double
@@ -25,7 +28,7 @@ final class Gif: NSImage {
                 images.append(tmpImage)
             }
         }
-        let calculatedDuration = Double(images.count) * delayTime
+        let calculatedDuration = Double(images.count) * (gifTime ?? pngTime)
         self.init()
         animatedImages = images
         self.calculatedDuration = calculatedDuration
